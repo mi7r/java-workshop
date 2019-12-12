d278288374e84af7f2c0f897a171967c89eef8b2
r <michalski.rados@gmail.com>
2019-12-12 20:09:43 +0100

    first commit

---
+++ src/test/java/com/workshop/java/reader/ReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.java.reader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
