32375ad2b89ed6c2cec3c14fce3211391ff4c9a4
r <michalski.rados@gmail.com>
2019-12-13 14:44:53 +0100

    CountryServiceImplTest added.

--- src/main/java/com/workshop/java/reader/domain/Country.java
+++ src/main/java/com/workshop/java/reader/domain/Country.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -11,6 +12,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class Country {
 
     @Id
---
+++ src/test/java/com/workshop/java/reader/service/CountryServiceImplTest.java
@@ -0,0 +1,51 @@
+package com.workshop.java.reader.service;
+
+import com.workshop.java.reader.domain.Country;
+import com.workshop.java.reader.exception.CountryCodeNotFoundException;
+import com.workshop.java.reader.repository.CountryRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class CountryServiceImplTest {
+
+    CountryServiceImpl countryService;
+
+    @Mock
+    CountryRepository countryRepository;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        countryService = new CountryServiceImpl(countryRepository);
+    }
+
+    @Test
+    public void getCountryByCountryCodeWithPositiveResult() {
+        Country country = new Country();
+        country.setCode("BHR");
+        Optional<Country> countryOptional = Optional.of(country);
+
+        when(countryRepository.findById(anyString())).thenReturn(countryOptional);
+
+        Optional<Country> countryToReturn = countryService.findCountryByCode("BHR");
+
+        assertNotNull(countryToReturn);
+        verify(countryRepository, times(1)).findById(anyString());
+        verify(countryRepository, never()).findAll();
+    }
+
+    @Test(expected = CountryCodeNotFoundException.class)
+    public void getCountryByCountryCodeThrowCountryCodeNotFoundException(){
+        Optional<Country> countryOptional = countryService.findCountryByCode("ZZZ");
+    }
+
+}
