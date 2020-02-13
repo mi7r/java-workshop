4827b94c560a39e69ce8bfe232a0caa62a1d0c52
radek <michalski.rados@gmail.com>
2020-02-13 11:42:34 +0100

    dependencies for groovy and spock added.

--- src/main/java/com/workshop/java/reader/exception/CityNotFoundException.java
+++ src/main/java/com/workshop/java/reader/exception/CityNotFoundException.java
@@ -4,8 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "INVALID_CITY_NAME")
-public class CityNotFoundException extends RuntimeException{
-        public CityNotFoundException(){
-                super();
-        }
+public class CityNotFoundException extends RuntimeException {
+
+    public CityNotFoundException() {
+        super();
+    }
 }
